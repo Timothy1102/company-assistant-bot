365fcfba326aa9e5ca8cbf9432ca81d70a44307c
Timothy1102 <thang.vu@kobizo.de>
2023-08-03 18:02:20 +0700

    Update dependencies for streamlit deployment

---
+++ README.md
@@ -0,0 +1,8 @@
+## Run it locally
+
+```sh
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+streamlit run ui.py
+```
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -10,30 +11,31 @@ import os
 load_dotenv()
 
 # 1. Vectorise the sales response csv data
-loader = CSVLoader (file_path="data.csv")
+loader = CSVLoader(file_path="data.csv")
 documents = loader.load()
 embeddings = OpenAIEmbeddings()
-db = FAISS.from_documents (documents, embeddings)
+# embeddings = HuggingFaceEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
 
 # 2. Function for similarity search
 def retrieve_info(query):
-    similar_response = db.similarity_search (query, k=3)
+    similar_response = db.similarity_search(query, k=3)
     page_contents_array = [doc.page_content for doc in similar_response]
     return page_contents_array
 
 # 3. Setup LLMChain & prompts
 template= """
-You are an assistant who helps customers understand our company. I will provide a question from a customer
-and you will give them the best answer based on some of our data.
+You are an assistant who helps me understand your company. I will provide a question
+and you will give me the best answer based on some data about your company.
 
-Below is a question I received from the customer:
+Below is my question:
 {message}
 
-Here is a list of related data about our company:
+Here is a list of related data about your company:
 {company_data}
-Please write the best response for the customer.
+Please write the best response.
 
-IMPORTANT NOTE: If the question is not related to our company, don't give direct answer but let the customer know that question is out-of-scope for this conversation and then get customer's attention back to the context of our company.
+IMPORTANT NOTE: If the question is not related to your company, don't give direct answer but let me know that question is out-of-scope for this conversation and then get my attention back to the context of your company.
 """
 
 prompt = PromptTemplate(
@@ -49,7 +51,7 @@ def generate_response(message):
     response = chain.run(message=message, company_data=company_data)
     return response
 
-# 5. test
+# 5. Test
 # message = """
 # why should I choose your service?
 # """
